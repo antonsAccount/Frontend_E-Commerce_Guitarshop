f97ccd0d73eb3f619aac8237f38f46c383b916a6
Anton <avonpoblozki@posteo.de>
2023-07-25 15:04:51 +0200

    add some very basic content to pages for better visibility during dev

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,20 @@
+import { Link } from "react-router-dom";
+
 type HomeProps = {
   token?: string;
 };
 
 export default function Home({ token }: HomeProps): JSX.Element {
   return (
-    <>
-      <h2 style={{ textAlign: "center" }}>Home Component</h2>
+    <div style={{ textAlign: "center" }}>
+      <h2>Home Component</h2>
       <br />
       <br />
       <br />
-      <br />
+      <h2>
+        Welcome to Anton's Rare Guitars. Feel free to check out all the
+        instruments we have in stock right now! <Link to="/shop">Here</Link>
+      </h2>
       <br />
       <br />
       <br />
@@ -50,6 +55,6 @@ export default function Home({ token }: HomeProps): JSX.Element {
       <br />
       <br />
       <h2>ar</h2>
-    </>
+    </div>
   );
 }
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,5 +1,22 @@
 type ShoppingCartProps = { stuff?: string };
 
 export default function ShoppingCart({ stuff }: ShoppingCartProps) {
-  return <div>Shopping Cart {stuff} </div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h2 style={{ textAlign: "center" }}>Shopping Cart Component {stuff} </h2>
+      <img
+        src={
+          "https://netrinoimages.s3.eu-west-2.amazonaws.com/2014/02/13/370112/438511/shopping_cart_v8_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_4515304_o.jpg"
+        }
+        style={{ width: "640px", height: "640px" }}
+      />
+    </div>
+  );
 }
